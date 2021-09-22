c608b78ccfea190ee25564ca94b027999e7b7d49
MonashBen <88640305+MonashBen@users.noreply.github.com>
2021-09-23 00:48:12 +1000

    Added parent controller class

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,4 +1,29 @@
 # Parent controller stub
+import tkinter as tk
+from views.login_view import LoginView
+from views.patient_home_view import PatientHomeView
+
 class Controller():
-    def __init__(self):
-        pass
+    def __init__(self, master: tk.Tk,view) -> None:
+        # master is an tk instance
+        self.__master = master
+        self.__set_controller(master)
+        self.__load_view(master)
+        self.__view = view
+        
+
+    def __set_controller(self, master: tk.Tk) -> None:
+        # set controller in tk instance
+        master.main_controller = self
+
+    def __load_view(self, master: tk.Tk) -> None:
+        # create new view
+        new_frame = self.__view(master)
+        # remove frame if tk instance has a frame
+        if master.main_frame is not None:
+            master.main_frame.destroy()
+        # assign new frame to tk instance
+        master.main_frame = new_frame
+        master.main_frame.grid_propagate(False)
+        master.main_frame.pack(side="top", fill="both", expand=True)  
+    pass
