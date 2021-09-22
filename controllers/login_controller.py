c810e21c8b92a6de4d1ddb35e6cd6240718a7d8b
MonashBen <88640305+MonashBen@users.noreply.github.com>
2021-09-23 00:48:41 +1000

    Changes to child controllers

--- controllers/login_controller.py
+++ controllers/login_controller.py
@@ -1,30 +1,13 @@
 import tkinter as tk
 from controllers.patient_home_controller import PatientHomeController
+from controllers.admin_home_controller import AdminHomeController
 from controllers.controller import Controller
 from views.login_view import LoginView
 import csv
 
 class LoginController(Controller):
-    def __init__(self, master: tk.Tk) -> None:
-        # master is an tk instance
-        self.__set_controller(master)
-        self.__load_view(master)
-        self.__master = master
-
-    def __set_controller(self, master: tk.Tk) -> None:
-        # set controller in tk instance
-        master.main_controller = self
-
-    def __load_view(self, master: tk.Tk) -> None:
-        # create new view
-        new_frame = LoginView(master)
-        # remove frame if tk instance has a frame
-        if master.main_frame is not None:
-            master.main_frame.destroy()
-        # assign new frame to tk instance
-        master.main_frame = new_frame
-        master.main_frame.grid_propagate(False)
-        master.main_frame.pack(side="top", fill="both", expand=True)
+    def __init__(self,master):
+        Controller.__init__(self,master,LoginView)
 
     def login(self, email_address: str, password: str):
         with open("./app_data/accounts.csv", "r", encoding='utf-8-sig') as f:
@@ -40,7 +23,7 @@ class LoginController(Controller):
                 # check if account details matches user login input
                 if account_email_address == email_address and account_password == password:
                     print("login successfully")
-                    self.__master.load_controller(PatientHomeController)
+                    #self.__master.load_controller(PatientHomeController)
                     return
         # TODO: Implement error feedback in view
         print("Incorrect email and password")
