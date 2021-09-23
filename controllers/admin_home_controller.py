c9fa4c1bbec3ac19c58a3df69dce762cac1356bb
MonashBen <88640305+MonashBen@users.noreply.github.com>
2021-09-23 14:53:22 +1000

    Minor constructor consistency change

--- controllers/admin_home_controller.py
+++ controllers/admin_home_controller.py
@@ -3,5 +3,5 @@ from controllers.controller import Controller
 from views.admin_home_view import AdminHomeView
 
 class AdminHomeController(Controller):
-    def __init__(self,master):
-        Controller.__init__(self,master,AdminHomeView)
+    def __init__(self,master,view = AdminHomeView):
+        Controller.__init__(self,master,view)
