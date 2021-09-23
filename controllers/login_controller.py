a42e5c95c774545b39e94eb381466e083c4c615d
MonashBen <88640305+MonashBen@users.noreply.github.com>
2021-09-23 14:52:17 +1000

    Added home page differentiation based on role

--- controllers/login_controller.py
+++ controllers/login_controller.py
@@ -13,18 +13,27 @@ class LoginController(Controller):
         with open("./app_data/accounts.csv", "r", encoding='utf-8-sig') as f:
             f_reader = csv.reader(f)
             header = next(f_reader, None)
+
             # find index of email address and password in header
             email_address_index = header.index("email_address")
             password_index = header.index("password")
+            type_index = header.index("type")
+
             for account_details in f_reader:
                 # extract account email address and password
                 account_email_address = account_details[email_address_index]
                 account_password = account_details[password_index]
+                account_type = account_details[type_index]
                 # check if account details matches user login input
                 if account_email_address == email_address and account_password == password:
-                    print("login successfully")
-                    self._master.load_controller(PatientHomeController)
+                    if account_type == 'administrator':
+                        self._master.load_controller(AdminHomeController)
+                        print("login successfully")
+                    elif account_type == 'patient':
+                        self._master.load_controller(PatientHomeController)
+                        print("login successfully")
                     return
+
         # TODO: Implement error feedback in view
         print("Incorrect email and password")
 
