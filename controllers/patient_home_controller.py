ef14154ba600904e3b2726c045310a5f044ff938
MonashBen <88640305+MonashBen@users.noreply.github.com>
2021-09-23 15:25:55 +1000

    Added template transition function

--- controllers/patient_home_controller.py
+++ controllers/patient_home_controller.py
@@ -5,3 +5,8 @@ from views.patient_home_view import PatientHomeView
 class PatientHomeController(Controller):
     def __init__(self,master,view = PatientHomeView):
         Controller.__init__(self,master,view)
+
+    def book_appointment(self):
+        #self._master.load_controller()
+        pass
+        
