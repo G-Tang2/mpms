4f384e9135f772ade87cf612c7c47b6edc883efd
MonashBen <88640305+MonashBen@users.noreply.github.com>
2021-09-23 15:25:25 +1000

    Added transition function to admin controller

--- controllers/admin_home_controller.py
+++ controllers/admin_home_controller.py
@@ -1,7 +1,12 @@
 import tkinter as tk
 from controllers.controller import Controller
 from views.admin_home_view import AdminHomeView
+from controllers.status_report_controller import StatusReportController
+
 
 class AdminHomeController(Controller):
     def __init__(self,master,view = AdminHomeView):
         Controller.__init__(self,master,view)
+
+    def status_report(self):
+        self._master.load_controller(StatusReportController)
