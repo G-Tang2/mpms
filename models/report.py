7a32aea84f9d2cf86a8c64f5c1c16998ce4d7234
MonashBen <88640305+MonashBen@users.noreply.github.com>
2021-09-26 12:30:10 +1000

    Added report code

--- models/report.py
+++ models/report.py
@@ -1,14 +1,32 @@
 
+from models.apppointment_list import AppointmentList
+
 class Report():
     def __init__(self, type):
         self.type = type
         self.statistic = {}
+        self.list_of_appointments = self.__fetch_appointment_list()
 
     def calculate_statistic(self, start_date, end_date, info = None):
-        if self.type == "reason":
-            # reason
-            pass
-    
+    #count
+        list_of_reasons = ["General Check-up","Reason2","Reason3"]
+        reason_list = [] 
+        # Append all reasons used in appointment to list
+        for appointment in self.list_of_appointments:
+            reason_list.append(appointment.get_reason())
+        #numpy
+
+        total = len(reason_list)
+
+        # Count occurence of each reason
+        for reason in list_of_reasons:
+            reason_count = reason_list.count(reason)
+            reason_percent = reason_count/total
+            self.statistic[reason] = reason_percent
+ 
     def get_statistic(self):
         return self.statistic
+    
+    def __fetch_appointment_list(self):
+        return AppointmentList()
         
--- views/status_report_view.py
+++ views/status_report_view.py
@@ -13,18 +13,21 @@ class StatusReportView(tk.Frame):
         tk.Label(self, text="Monash Clinic", font=('Roboto',44), bg = '#67b9e6',width=500).pack()
         tk.Label(self, text = "").pack()
 
-        options = ['Mon','Tue','Wed']
+        options = ['GP','Reason','Clinic']
         #valueinside = tk.StringVar(self)
+        start_date = tk.StringVar()
+        end_date = tk.StringVar()
+        report_type = tk.StringVar()
 
         # 'Book Appointment' Button
         tk.Label(self, text = "Start Date", width=18, height=5).pack()
-        tk.OptionMenu(self,tk.StringVar(self),*options).pack()
+        tk.OptionMenu(self,start_date,*options).pack()
         tk.Label(self, text = "").pack()
 
         tk.Label(self, text = "End Date", width=18, height=5).pack()
-        tk.OptionMenu(self,tk.StringVar(self),*options).pack()
+        tk.OptionMenu(self,end_date,*options).pack()
         tk.Label(self, text = "").pack()
 
         tk.Label(self, text = "Report Type", width=18, height=5).pack()
-        tk.OptionMenu(self,tk.StringVar(self),*options).pack()
+        tk.OptionMenu(self,report_type,*options).pack()
         tk.Label(self, text = "").pack()
