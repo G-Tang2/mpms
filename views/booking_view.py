bbe130762b4aedaaefa6392767f514bfb3f1733e
whitglaze <cxuu0038@student.monash.edu>
2021-09-27 21:55:52 +0800

    booking_view version2 with button function

--- views/booking_view.py
+++ views/booking_view.py
@@ -13,7 +13,11 @@ class BookView(tk.Frame):
 
         # page content
         tk.Label(self, text='Monash Clinic', width=800, height=2, bg='lightskyblue').pack()
-        tk.Button(self, text='show_branches', width=15, height=2).pack()
-        tk.Listbox(self).pack()
-        tk.Button(self, text='show_selection', width=15, height=2).pack()
-        tk.Label(self, width=800, height=2, bg='lightskyblue').pack()
+        listbox = tk.Listbox(self)
+        label = tk.Label(self, width=800, height=2, bg='lightskyblue')
+        tk.Button(self, text='show_branches', width=15, height=2,
+                  command=lambda: master.main_controller.show_branches(listbox)).pack()
+        listbox.pack()
+        tk.Button(self, text='show_selection', width=15, height=2,
+                  command=lambda: master.main_controller.show_selection(listbox, label)).pack()
+        label.pack()
