2a4a6bbb90f9ec77f168ae1b70dab07c74ce273b
Garvin <gtan36@student.monash.edu>
2021-10-03 15:18:59 +1100

    Private user attributes

--- models/user.py
+++ models/user.py
@@ -1,21 +1,21 @@
 
 class User():
     def __init__(self, email_address: str, password: str) -> None:
-        self.email_address = email_address
-        self.password = password
+        self._email_address = email_address
+        self._password = password
 
     def get_email_address(self) -> str:
-        return self.email_address
+        return self._email_address
 
     def set_email_address(self, email_address: str) -> bool:
-        self.email_address = email_address
+        self._email_address = email_address
         return True
 
     def get_password(self) -> str:
-        return self.password 
+        return self._password 
 
     def set_password(self, password: str) -> None:
-        self.password = password
+        self._password = password
         return True
 
     
