c06117608d9668e2138708b87f24d09170b0c35f
Garvin <gtan36@student.monash.edu>
2021-10-04 03:51:21 +1100

    Changed valid_credentials return type from dictionary to tuple

--- models/login.py
+++ models/login.py
@@ -10,12 +10,12 @@ class Login():
         self.user = self.load_user(email_address, password)
         
     def load_user(self, email_address: str, password: str) -> User:
-        res = self.valid_credentials(email_address, password)
-        if res["is_valid"]:
-            if res["account_type"] == "administrator":
-                return Admin(res["user_details"])
-            elif res["account_type"] == "patient":
-                return Patient(res["user_details"])
+        is_valid_user, account_type, account_details = self.valid_credentials(email_address, password)
+        if is_valid_user:
+            if account_type == "administrator":
+                return Admin(account_details)
+            elif account_type == "patient":
+                return Patient(account_details)
             else:
                 # TODO: Throw error
                 print("Invalid account type")
@@ -23,6 +23,10 @@ class Login():
             raise ValueError("Invalid email address or password")
 
     def valid_credentials(self, email_address: str, password: str) -> bool:
+        # default values
+        is_valid_user = False
+        account_type = None
+        account_details = None
         with open("./app_data/accounts.csv", "r", encoding='utf-8-sig') as f:
             f_reader = csv.reader(f)
             header = next(f_reader, None)
@@ -37,29 +41,22 @@ class Login():
                 # extract account email address and password
                 account_email_address = account_details[email_address_index]
                 account_password = account_details[password_index]
-                account_type = account_details[type_index]
-                account_user_detail = account_details[user_detail_index]
 
                 # check if account details matches user login input
                 if account_email_address == email_address and account_password == password:
+                    is_valid_user = True
+                    account_type = account_details[type_index]
+                    account_user_detail = account_details[user_detail_index]
+
                     # pack email address and password into user details and format date of birth
-                    user_details = json.loads(account_user_detail)
-                    user_details["email_address"] = email_address
-                    user_details["password"] = password
-                    if "date_of_birth" in user_details:
-                        user_details["date_of_birth"] = datetime.strptime(user_details["date_of_birth"], "%d/%m/%Y").date()
-                    return_dict = {
-                        "is_valid": True,
-                        "account_type": account_type,
-                        "user_details": user_details
-                    }
-                    return return_dict
-            return_dict = {
-                "is_valid": False,
-                "account_type": None,
-                "user_detail": None
-            }
-            return return_dict
+                    account_details = json.loads(account_user_detail)
+                    account_details["email_address"] = email_address
+                    account_details["password"] = password
+                    if "date_of_birth" in account_details:
+                        account_details["date_of_birth"] = datetime.strptime(account_details["date_of_birth"], "%d/%m/%Y").date()
+                    break
+            
+            return is_valid_user, account_type, account_details
 
     def is_patient(self):
         return isinstance(self.user, Patient)
