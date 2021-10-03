423c1dfea906a921dd119608ccda6ab25d117ed7
Garvin <gtan36@student.monash.edu>
2021-10-03 15:36:49 +1100

    Change admin parameters to accept json

--- models/admin.py
+++ models/admin.py
@@ -1,5 +1,5 @@
 from models.user import User
 
 class Admin(User):
-    def __init__(self, email_address: str, password: str) -> None:
-        User.__init__(self, email_address, password)
+    def __init__(self, arg) -> None:
+        User.__init__(self, arg["email_address"], arg["password"])
