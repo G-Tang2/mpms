f24132eb1c38aa4da64277f7c7ff0593948ce803
MonashBen <88640305+MonashBen@users.noreply.github.com>
2021-10-04 14:24:54 +1100

    Updated view

--- views/status_report_view.py
+++ views/status_report_view.py
@@ -10,51 +10,66 @@ from matplotlib.figure import Figure
 class StatusReportView(tk.Frame):
     def __init__(self, master: tk.Tk) -> None:
         # Initialise frame and set controller
-        tk.Frame.__init__(self, master)
+        tk.Frame.__init__(self, master, bg="#c1e4f7")
         self.pack_propagate(False)
         self.pack(side="top", fill="both", expand=True)
         self.__render_view(master)
-        self.reason_table = None
        
 
     def __render_view(self, master: tk.Tk) -> None:
         # 'Monash Clinic' Header
-        tk.Label(self, text="Monash Clinic", font=('Roboto',44), bg = '#67b9e6',width=500).pack()
-        tk.Label(self, text = "").pack()
+        tk.Label(self, text="   Monash Clinic", font=('Roboto',38, "bold"), anchor="w", bg="white").pack(ipady=10, fill="x")
+        
+        # divider
+        tk.Frame(self, bg="black", height=2).pack(fill="x")
+
+        # container for login details
+        outer_label_frame = tk.LabelFrame(self, relief="solid", borderwidth=2, bg="white")
+
+        inner_label_frame = tk.LabelFrame(outer_label_frame, relief="flat", bg="white")
 
         # 'Start' Date
-        tk.Label(self, text = "Start Date", width=18, height=1).pack()
-        start_cal = Calendar(self,selectmode = "day", year = 2021, month = 5, day = 22, date_pattern ='yyyy-mm-dd', selectforeground ='#67b9e6')
+        tk.Label(inner_label_frame, text = "Start Date", width=18, height=1).pack()
+        start_cal = Calendar(inner_label_frame,selectmode = "day", year = 2021, month = 5, day = 22, date_pattern ='yyyy-mm-dd', selectforeground ='#67b9e6')
         start_cal.pack()
-        tk.Label(self, text = "", width=18, height= 2).pack()
+        tk.Label(inner_label_frame, text = "", width=18, height= 2).pack()
     
         # End Date
-        tk.Label(self, text = "End Date", width=18, height=1).pack()
-        end_cal = Calendar(self,selectmode = "day", year = 2021, month = 5, day = 22, date_pattern ='yyyy-mm-dd', selectforeground ='#67b9e6')
+        tk.Label(inner_label_frame, text = "End Date", width=18, height=1).pack()
+        end_cal = Calendar(inner_label_frame,selectmode = "day", year = 2021, month = 5, day = 22, date_pattern ='yyyy-mm-dd', selectforeground ='#67b9e6')
         end_cal.pack()
-        tk.Label(self, text = "", width=18, height= 2).pack()
+        tk.Label(inner_label_frame, text = "", width=18, height= 2).pack()
         
         # Report Type
         report_options = ['Reason']
         report_type = tk.StringVar()
         report_type.set(report_options[0])
 
-        tk.Label(self, text = "Report Type", width=18, height=1).pack()
-        tk.OptionMenu(self,report_type,*report_options).pack()
-        tk.Label(self, text = "").pack()
+        tk.Label(inner_label_frame, text = "Report Type", width=18, height=1).pack()
+        tk.OptionMenu(inner_label_frame,report_type,*report_options).pack()
+        tk.Label(inner_label_frame, text = "").pack()
 
+        tk.Button(inner_label_frame,text="Generate Report",command = lambda: self.display_reason_report(start_cal.get_date(),end_cal.get_date(),report_type.get())).pack()
     
+        inner_label_frame.pack(padx=50, fill="x")
+        outer_label_frame.pack(padx=350, pady=50, fill="x")
 
-        tk.Button(self,text="Generate Report",command = lambda: self.display_reason_report(start_cal.get_date(),end_cal.get_date(),report_type.get())).pack()
-    
     def display_reason_report(self, start_date, end_date, report_type):
+        
+        if start_date >= end_date:
+            tk.messagebox.showerror("Error", "Start date should be earlier than end date")
+            return None
         
         reason_dict = self.master.main_controller.get_report_statistic(start_date,end_date,report_type)
 
+        if len(reason_dict) == 0:
+            tk.messagebox.showerror("Error", "No appointments within this period")
+            return
+
         report = Toplevel(self)
         report.title("Reason Report")
-        report.geometry("700x700")
-        
+        report.geometry("500x500")
+
         tk.Label(report,text = "Reason Pie Chart").pack()
         f = Figure(figsize=(5,4), dpi= 100)
         a = f.add_subplot(111)
@@ -64,25 +79,20 @@ class StatusReportView(tk.Frame):
         canvas = FigureCanvasTkAgg(f,report)
         canvas.get_tk_widget().pack(fill=tk.BOTH)
         
-        
-        #Create table
-        if self.reason_table is not None:
-            self.reason_table.destroy()
-        
         # Add reason table
-        self.reason_table = ttk.Treeview(report, height = len(reason_dict))
-        self.reason_table['columns'] = ("Reason","Occurences")
+        reason_table = ttk.Treeview(report, height = len(reason_dict))
+        reason_table['columns'] = ("Reason","Occurences")
 
-        self.reason_table.column("#0",width=0,stretch=NO)
-        self.reason_table.column("Reason",anchor=W, width = 120)
-        self.reason_table.column("Occurences",anchor=E, width=120)
+        reason_table.column("#0",width=0,stretch=NO)
+        reason_table.column("Reason",anchor=W, width = 120)
+        reason_table.column("Occurences",anchor=E, width=120)
 
-        self.reason_table.heading("#0", text = '', anchor=W)
-        self.reason_table.heading("Reason", text = 'Reason', anchor=W)
-        self.reason_table.heading("Occurences", text = 'Occurence Percentage (%)', anchor=E)
+        reason_table.heading("#0", text = '', anchor=W)
+        reason_table.heading("Reason", text = 'Reason', anchor=W)
+        reason_table.heading("Occurences", text = 'Occurence Percentage (%)', anchor=E)
     
         table_iid = 0
         for key,value in reason_dict.items():
-            self.reason_table.insert(parent='',index='end',iid=table_iid,text="",values=(key,round(value,2)))
+            reason_table.insert(parent='',index='end',iid=table_iid,text="",values=(key,round(value,2)))
             table_iid += 1
-        self.reason_table.pack(fill=tk.BOTH)
+        reason_table.pack(fill=tk.BOTH)
