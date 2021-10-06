264f8251cbcae9def0592a2db0af9ea5d6ceb2a5
Garvin <gtan36@student.monash.edu>
2021-10-07 00:47:28 +1100

    Decomposed dictionary fields to class parameter in branch class

--- models/branch.py
+++ models/branch.py
@@ -4,16 +4,17 @@ from models.apppointment_list import AppointmentList
 import json
 
 class Branch():
-    def __init__(self, info):
-            self.name = info["name"]
-            self.address = info["address"]
-            self.open_hour = info["open_hour"]
-            self.close_hour = info["close_hour"]
-            self.phone_number = info["phone_number"]
-            self.unavailable_days = info["unavailable_days"]
-            self.appointments = AppointmentList(json.loads(info["appointments"]))
-            self.queue = Queue()
-            self.gps = GPList(json.loads(info["gps"]))
+    def __init__(self, name, address, open_hour, close_hour, 
+        phone_number, unavailable_days,appointments, gps, queue = Queue()):
+            self.name = name
+            self.address = address
+            self.open_hour = open_hour
+            self.close_hour = close_hour
+            self.phone_number = phone_number
+            self.unavailable_days = unavailable_days
+            self.appointments = AppointmentList(json.loads(appointments))
+            self.gps = GPList(json.loads(gps))
+            self.queue = queue
     
     def get_name(self):
         return self.name
--- models/branch_list.py
+++ models/branch_list.py
@@ -11,7 +11,9 @@ class BranchList():
         with open("./app_data/branches.csv", "r", encoding='utf-8-sig') as f:
             f_reader = csv.DictReader(f)
             for branch_info in f_reader:
-                branch_list.append(Branch(branch_info))
+                branch_list.append(Branch(branch_info["name"], branch_info["address"],
+                    branch_info["open_hour"], branch_info["close_hour"], branch_info["phone_number"],
+                    branch_info["unavailable_days"], branch_info["appointments"], branch_info["gps"]))
 
         return branch_list
 
