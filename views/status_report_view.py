73e8c623409d03046f9c80f9d56ad622447901e5
Garvin <gtan36@student.monash.edu>
2021-10-07 17:43:09 +1100

    Changed calendar to be date entry

--- views/status_report_view.py
+++ views/status_report_view.py
@@ -1,3 +1,4 @@
+from calendar import weekheader
 import tkinter as tk
 from tkinter import Toplevel, ttk
 from tkinter.constants import E, NO, W
@@ -20,16 +21,22 @@ class StatusReportView(tk.Frame):
         outer_label_frame = tk.LabelFrame(self, relief="solid", borderwidth=2, bg="white")
         inner_label_frame = tk.LabelFrame(outer_label_frame, relief="flat", bg="white")
 
-        
+        date_label_frame = tk.LabelFrame(inner_label_frame, relief="flat", bg="white")
         # 'Start' Date
-        tk.Label(inner_label_frame, text = "Start Date", width=18, height=1, bg="white").pack()
-        start_cal = Calendar(inner_label_frame,selectmode = "day", year = 2021, month = 5, day = 22, date_pattern ='yyyy-mm-dd', selectforeground ='#67b9e6', showweeknumbers = False)
+        start_date_label_frame = tk.LabelFrame(date_label_frame, relief="flat", bg="white")
+        tk.Label(start_date_label_frame, text = "Start Date", width=18, height=1, bg="white").pack()
+        start_cal = DateEntry(start_date_label_frame, selectmode = 'day', showweeknumbers = False)
         start_cal.pack()
     
         # End Date
-        tk.Label(inner_label_frame, text = "End Date", width=18, height=1, bg="white").pack()
-        end_cal = Calendar(inner_label_frame,selectmode = "day", year = 2021, month = 5, day = 22, date_pattern ='yyyy-mm-dd', selectforeground ='#67b9e6', showweeknumbers = False)
+        end_date_label_frame = tk.LabelFrame(date_label_frame, relief="flat", bg="white")
+        tk.Label(end_date_label_frame, text = "End Date", width=18, height=1, bg="white").pack()
+        end_cal = DateEntry(end_date_label_frame, selectmode = 'day', showweeknumbers = False)
         end_cal.pack()
+
+        start_date_label_frame.pack(side="left")
+        end_date_label_frame.pack(side="right")
+        date_label_frame.pack()
         
         # Report Type
         report_options = ['Reason']
@@ -39,7 +46,8 @@ class StatusReportView(tk.Frame):
         tk.Label(inner_label_frame, text = "Report Type", width=18, height=1, bg="white").pack()
         tk.OptionMenu(inner_label_frame,report_type,*report_options).pack()
 
-        tk.Button(inner_label_frame,text="Generate Report",command = lambda: self.controller.get_reason_report(start_cal.get_date(),end_cal.get_date(),report_type.get())).pack()
+        tk.Button(inner_label_frame,text="Generate Report",
+            command = lambda: self.controller.get_reason_report(start_cal.get_date(),end_cal.get_date(),report_type.get())).pack()
     
         inner_label_frame.pack(padx=50, fill="x")
         outer_label_frame.pack(padx=350, pady=50, fill="x")
