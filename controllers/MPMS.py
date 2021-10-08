cf7fd8841cec063b0fb24ee3fc7cdd6d8f7e7eed
Garvin <gtan36@student.monash.edu>
2021-10-08 23:38:39 +1100

    Change MPMS into a singleton

--- controllers/MPMS.py
+++ controllers/MPMS.py
@@ -5,7 +5,6 @@ import tkinter as tk
 # from views.patient_home_view import PatientHomeView
 from models.branch_list import BranchList
 
-
 class MPMS():
     def __init__(self, master: tk.Tk, view) -> None:
         # master is an tk instance
@@ -13,6 +12,7 @@ class MPMS():
         self._view = view(master, self)
         self.list_of_branches = BranchList.create_from_csv()
         self.__load_view(master)
+        instance = None
         # list_of_appointments = self.__fetch_appointment_list()
 
     def __load_view(self, master: tk.Tk) -> None:
@@ -29,4 +29,10 @@ class MPMS():
 
     def get_list_of_branches(self):
         return self.list_of_branches
+
+    @staticmethod
+    def get_instance():
+        if MPMS.instance is None:
+            MPMS.instance = MPMS()
+        return MPMS.instance
     
