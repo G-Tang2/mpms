1e6d5caf0166d4a38c7474f04adb4b693243181a
MonashBen <88640305+MonashBen@users.noreply.github.com>
2021-10-11 00:18:35 +1100

    Added home and logout button to view

--- views/header_view.py
+++ views/header_view.py
@@ -1,20 +1,17 @@
 import tkinter as tk
 from controllers.patient_home_controller import PatientHomeController
 from controllers.admin_home_controller import AdminHomeController
+from controllers.login_controller import LoginController
 
 class Header(tk.Frame):
     def __init__(self, master: tk.Tk):
         tk.Frame.__init__(self, master)
         # header
-        tk.Button(self, text="   Monash Clinic", relief = 'flat', command = lambda: self.return_home(),borderwidth= 0, font=('Roboto',38, "bold"), anchor="w", bg="#FFFFFF").pack(ipady=10, fill="x")      
+        header_frame = tk.LabelFrame(self)
+        tk.Button(header_frame, text="   Monash Clinic", relief = 'flat', command = lambda: master.header_controller.return_home(),borderwidth= 0, highlightthickness = 0 , font=('Roboto',38, "bold"), anchor="w", bg="red").pack(side = "left", ipady=5)      
+        tk.Button(header_frame, text = "Logout", relief = 'flat', command = lambda: master.header_controller.logout(),borderwidth= 0, highlightthickness = 0 , font=('Roboto',38, "bold"), anchor="w", bg="#99d2f2").pack(side = "right", ipady=5)
+        header_frame.pack(fill = 'x')
         # divider
-        tk.Frame(self, bg="black", height=2).pack(fill="x")
+        tk.Frame(self, bg="black", height=2).pack(side = "bottom", fill="x")
         
-
-    def return_home(self):
-        if self.master.login.is_patient():
-            self.master.load_controller(PatientHomeController) 
-            print("Patient")
-        else:
-            self.master.load_controller(AdminHomeController) 
-            print("Admin")
+    
