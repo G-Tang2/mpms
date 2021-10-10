81625f96d9b3f042964224db0f6652881bc900e7
whitglaze <cxuu0038@student.monash.edu>
2021-10-10 20:48:56 +0800

    10/10 remove indent from json file

--- utils/json.py
+++ utils/json.py
@@ -4,4 +4,4 @@ class JSON():
     @staticmethod
     def to_JSON(obj):
         return json.dumps(obj, default=lambda o: o.__dict__, 
-            sort_keys=True, indent=4)
+            sort_keys=True)
