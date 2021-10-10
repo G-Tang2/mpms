03390a889daf857558348464d0cbe37d47695677
MonashBen <88640305+MonashBen@users.noreply.github.com>
2021-10-10 23:54:45 +1100

    Controller uses MPMS instance

--- controllers/status_report_controller.py
+++ controllers/status_report_controller.py
@@ -11,7 +11,7 @@ from tkinter.constants import E, NO, W
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
-import datetime
+from datetime import datetime
 
 class StatusReportController(Controller):
     def __init__(self,master):
@@ -35,7 +35,7 @@ class StatusReportController(Controller):
             self._view.display_input_error("Start date should be earlier than end date")
             return
            
-        reason_dict = self.get_report_statistic(start_date,end_date,report_type)
+        reason_dict = self.return_statistic(start_date,end_date,report_type)
 
         if len(reason_dict) == 0:
             self._view.display_input_error("No appointments within this period")
@@ -43,6 +43,34 @@ class StatusReportController(Controller):
         
         self._view.display_reason_report(reason_dict)
 
-    def get_report_statistic(self, start_date, end_date,report_type):
-        report = Report(report_type)
-        return report.get_statistic(start_date, end_date, report_type)
+    def return_statistic(self, start_date, end_date, report_type, info = None):
+        self.statistic = {}
+
+        mpms_instance = MPMS.get_instance()
+        branch_list= mpms_instance.get_list_of_branches()
+        list_of_branches = branch_list.get_branch_list()
+
+        if report_type.lower() == "reason":
+            
+            start_date = datetime.strptime(str(start_date), '%Y-%m-%d')
+            end_date = datetime.strptime(str(end_date), '%Y-%m-%d')
+
+            total_reasons = 0
+            for branch in list_of_branches:
+                appointment_list = branch.get_appointments()
+                appointment_list = appointment_list.get_appointment_list()
+                # Filter between time periods
+                for appointment in appointment_list:
+                    # Filter between time periods
+                    appointment_date = appointment.get_appointment_datetime()
+                    if start_date <  appointment_date < end_date:
+                        total_reasons += 1
+                        appointment_reason_object = appointment.get_appointment_reason()
+                        appointment_reason = appointment_reason_object.get_reason()
+                        if appointment_reason not in self.statistic:
+                            self.statistic[appointment_reason] = 1
+                        else:
+                            self.statistic[appointment_reason] += 1
+            for key,value in self.statistic.items():
+                self.statistic[key] = (value/total_reasons) * 100
+            return self.statistic
