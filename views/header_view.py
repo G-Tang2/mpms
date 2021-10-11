30f95a03363745118e937c731a99a23d90c7fc3e
MonashBen <88640305+MonashBen@users.noreply.github.com>
2021-10-11 15:26:59 +1100

    Header view button

--- views/header_view.py
+++ views/header_view.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from models.MPMS import MPMS
 from controllers.patient_home_controller import PatientHomeController
 from controllers.admin_home_controller import AdminHomeController
 from controllers.login_controller import LoginController
@@ -6,12 +7,19 @@ from controllers.login_controller import LoginController
 class Header(tk.Frame):
     def __init__(self, master: tk.Tk):
         tk.Frame.__init__(self, master)
+        self.logout_image = tk.PhotoImage(file = 'images/download.png')
+        self.master = master
         # header
-        header_frame = tk.LabelFrame(self)
-        tk.Button(header_frame, text="   Monash Clinic", relief = 'flat', command = lambda: master.header_controller.return_home(),borderwidth= 0, highlightthickness = 0 , font=('Roboto',38, "bold"), anchor="w", bg="red").pack(side = "left", ipady=5)      
-        tk.Button(header_frame, text = "Logout", relief = 'flat', command = lambda: master.header_controller.logout(),borderwidth= 0, highlightthickness = 0 , font=('Roboto',38, "bold"), anchor="w", bg="#99d2f2").pack(side = "right", ipady=5)
-        header_frame.pack(fill = 'x')
+        self.header_frame = tk.LabelFrame(self)
+        tk.Button(self.header_frame, text="   Monash Clinic", relief = 'flat', command = lambda: master.header_controller.return_home(),borderwidth= 0, highlightthickness = 0 , font=('Roboto',38, "bold"), anchor="w", bg="white").pack(side = "left", ipady=5)      
+        
+        
+        tk.Button(self.header_frame, image = self.logout_image, relief = 'flat', command = lambda: master.header_controller.logout(),borderwidth= 0, highlightthickness = 0 , height = 5, anchor="w", bg="#99d2f2").pack(side = "right", ipady=5)
+        self.header_frame.pack(fill = 'x')
         # divider
         tk.Frame(self, bg="black", height=2).pack(side = "bottom", fill="x")
+    
+    def refresh(self):
+        pass
         
     
