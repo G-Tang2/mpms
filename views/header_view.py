d0bcb2c10b2c15157818d776ff054dbc0e701278
MonashBen <88640305+MonashBen@users.noreply.github.com>
2021-10-11 23:33:54 +1100

    Added rremove logout button function

--- views/header_view.py
+++ views/header_view.py
@@ -10,18 +10,40 @@ class Header(tk.Frame):
         self.logout_image = tk.PhotoImage(file = 'images/download.png')
         self.master = master
         # header
-        self.header_frame = tk.LabelFrame(self)
-        tk.Button(self.header_frame, text="   Monash Clinic", relief = 'flat', command = lambda: master.header_controller.return_home(),borderwidth= 0, highlightthickness = 0 , font=('Roboto',38, "bold"), anchor="w", bg="white").pack(side = "left", ipady=5)      
+        self.header_frame = tk.Frame(self,bg='red')
+        tk.Button(self.header_frame, text="   Monash Clinic", relief = 'flat',borderwidth= 0, highlightthickness = 0 , font=('Roboto',38, "bold"), anchor="w", bg="white",
+            command = lambda: master.header_controller.return_home()).pack(side = "left", ipady=5)      
         self.header_frame.pack(fill = 'x')
-        
 
         # divider
         tk.Frame(self, bg="black", height=2).pack(side = "bottom", fill="x")
     
     def display_logout_btn(self):
-        self.header_frame.pack_forget()
-        tk.Button(self.header_frame, image = self.logout_image, relief = 'flat', command = lambda: self.master.header_controller.logout(),borderwidth= 0, highlightthickness = 0 , height = 5, anchor="w", bg="#99d2f2").pack(side = "right", ipady=5)
+        
+        self.pack(fill='x')
+        self.header_frame.destroy()
+        self.header_frame = tk.Frame(self,bg='white')
+
+        tk.Button(self.header_frame, text="   Monash Clinic", relief = 'flat',borderwidth= 0, highlightthickness = 0 , font=('Roboto',38, "bold"), anchor="w", bg="white",
+            command = lambda: self.master.header_controller.return_home()).pack(fill = 'x', side = "left", ipady=5)
+        tk.Button(self.header_frame, image = self.logout_image, relief = 'flat', 
+            command = lambda: self.master.header_controller.logout(),borderwidth= 0, highlightthickness = 0 , height = 5, anchor="w", bg="#99d2f2").pack(fill = 'x',side = "right", ipady=5) 
+
+        self.header_frame.pack(fill = 'x')
+    
+    def hide_logout_btn(self):
+        self.pack(fill='x')
+        self.header_frame.destroy()
+        self.header_frame = tk.Frame(self,bg='white')
+
+        tk.Button(self.header_frame, text="   Monash Clinic", relief = 'flat',borderwidth= 0, highlightthickness = 0 , font=('Roboto',38, "bold"), anchor="w", bg="white",
+            command = lambda: self.master.header_controller.return_home()).pack(fill = 'x', side = "left", ipady=5)
+        
         self.header_frame.pack(fill = 'x')
 
-        
+        #self.header_frame.pack_forget()
+        #tk.Button(self.header_frame, image = self.logout_image, relief = 'flat', 
+         #   command = lambda: self.master.header_controller.logout(),borderwidth= 0, highlightthickness = 0 , height = 5, anchor="w", bg="#99d2f2").pack(side = "right", ipady=5)
+        #self.header_frame.pack(fill = 'x')
+
     
