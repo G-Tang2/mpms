a9355f0f2ba994adf130ce4a1da4eca5910c4072
Garvin <gtan36@student.monash.edu>
2021-10-12 18:09:04 +1100

    Rearrange logout button code

--- views/header_view.py
+++ views/header_view.py
@@ -9,8 +9,6 @@ class Header(tk.Frame):
         tk.Frame.__init__(self, master)
         self.logout_image = tk.PhotoImage(file = 'images/download.png')
         self.master = master
-        self.logout_btn = tk.Button(self.header_frame, image = self.logout_image, relief = 'flat', 
-            command = lambda: self.master.header_controller.logout(),borderwidth= 0, highlightthickness = 0 , height = 5, anchor="w", bg="#99d2f2")
         # header
         self.header_frame = tk.Frame(self,bg='white')
         tk.Button(self.header_frame, text="   Monash Clinic", relief = 'flat',borderwidth= 0, highlightthickness = 0 , font=('Roboto',38, "bold"), anchor="w", bg="white",
@@ -19,6 +17,10 @@ class Header(tk.Frame):
 
         # divider
         tk.Frame(self, bg="black", height=2).pack(side = "bottom", fill="x")
+
+        # logout button
+        self.logout_btn = tk.Button(self.header_frame, image = self.logout_image, relief = 'flat', 
+            command = lambda: self.master.header_controller.logout(),borderwidth= 0, highlightthickness = 0 , height = 5, anchor="w", bg="#99d2f2")
     
     def display_logout_btn(self):
         self.logout_btn.pack(fill = 'x',side = "right", ipady=5, padx = 10) 
