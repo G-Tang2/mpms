dcbc480182b2787f48a691af5e0a1ec507c90b11
Garvin <gtan36@student.monash.edu>
2021-10-12 17:55:28 +1100

    Rearranged code for readability

--- views/header_view.py
+++ views/header_view.py
@@ -9,12 +9,12 @@ class Header(tk.Frame):
         tk.Frame.__init__(self, master)
         self.logout_image = tk.PhotoImage(file = 'images/download.png')
         self.master = master
+        self.logout_btn = tk.Button(self.header_frame, image = self.logout_image, relief = 'flat', 
+            command = lambda: self.master.header_controller.logout(),borderwidth= 0, highlightthickness = 0 , height = 5, anchor="w", bg="#99d2f2")
         # header
         self.header_frame = tk.Frame(self,bg='white')
         tk.Button(self.header_frame, text="   Monash Clinic", relief = 'flat',borderwidth= 0, highlightthickness = 0 , font=('Roboto',38, "bold"), anchor="w", bg="white",
             command = lambda: master.header_controller.return_home()).pack(side = "left", ipady=5)
-        self.logout_btn = tk.Button(self.header_frame, image = self.logout_image, relief = 'flat', 
-            command = lambda: self.master.header_controller.logout(),borderwidth= 0, highlightthickness = 0 , height = 5, anchor="w", bg="#99d2f2")
         self.header_frame.pack(fill = 'x')
 
         # divider
