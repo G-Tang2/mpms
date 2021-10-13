250aed81321df75e3c3305dae8993fdd2a1d7d20
Garvin <gtan36@student.monash.edu>
2021-10-13 21:09:43 +1100

    Use datetime to compare date values

--- controllers/status_report_controller.py
+++ controllers/status_report_controller.py
@@ -10,27 +10,28 @@ class StatusReportController(Controller):
         self._view = StatusReportView(master, self)
         self._view.render_view()
         self._load_view()
+        self.MPMS = MPMS.get_instance() 
 
-    def get_reason_report(self, start_date, end_date, report_type):
+    def get_reason_report(self, start_date: str, end_date: str, report_type: str):
         '''
         Validates the status-report view input. Returns errors message if invalid.
         Calls view function to display the reason report
         '''
         # Validate the format of date input
         try:
-            start_date_check = datetime.datetime.strptime(start_date, '%d/%m/%Y')
-            end_date_check = datetime.datetime.strptime(end_date, '%d/%m/%Y')
+            start_datetime = datetime.datetime.strptime(start_date, '%d/%m/%Y')
+            end_datetime = datetime.datetime.strptime(end_date, '%d/%m/%Y')
         except ValueError:
             self._view.display_input_error("Incorrect date format input \n(Use DD/MM/YYYY)")
             return
         # Validates start date occurs before end date
-        if start_date >= end_date:
-            
+        if start_datetime > end_datetime:
+
             self._view.display_input_error("Start date should be earlier than end date")
             return
         # Use MPMS instance and calls function to obtain reason statistic from MPMS
-        mpms_instance = MPMS.get_instance()  
-        reason_dict = mpms_instance.calculate_reason_statistic(start_date,end_date,report_type)
+        if report_type.lower() == "reason":
+            reason_dict = self.MPMS.calculate_reason_statistic(start_datetime, end_datetime)
         # Returns error if no appointments occured with give time period 
         if len(reason_dict) == 0:
             self._view.display_input_error("No appointments within this period")
--- models/MPMS.py
+++ models/MPMS.py
@@ -37,35 +37,31 @@ class MPMS:
             if branch_name == branch.get_name():
                 return branch
 
-    def calculate_reason_statistic(self, start_date, end_date, report_type, info = None):
-        self.statistic = {}
+    def calculate_reason_statistic(self, start_date: datetime, end_date: datetime) -> dict:
+        statistic = {}
 
         list_of_branches = self.list_of_branches.get_branch_list()
 
-        if report_type.lower() == "reason":
-            
-            start_date = datetime.datetime.strptime(str(start_date), '%d/%m/%Y')
-            end_date = datetime.datetime.strptime(str(end_date), '%d/%m/%Y')
-
-            total_reasons = 0
-            for branch in list_of_branches:
-                appointment_list = branch.get_appointments()
-                appointment_list = appointment_list.get_appointment_list()
+        total_reasons = 0
+        for branch in list_of_branches:
+            appointment_list = branch.get_appointments()
+            appointment_list = appointment_list.get_appointment_list()
+            # Filter between time periods
+            for appointment in appointment_list:
                 # Filter between time periods
-                for appointment in appointment_list:
-                    # Filter between time periods
-                    appointment_date = appointment.get_appointment_datetime()
-                    if start_date <  appointment_date < end_date:
-                        total_reasons += 1
-                        appointment_reason_object = appointment.get_appointment_reason()
-                        appointment_reason = appointment_reason_object.get_reason()
-                        if appointment_reason not in self.statistic:
-                            self.statistic[appointment_reason] = 1
-                        else:
-                            self.statistic[appointment_reason] += 1
-            for key,value in self.statistic.items():
-                self.statistic[key] = (value/total_reasons) * 100
-            return self.statistic
+                appointment_date = appointment.get_appointment_datetime()
+                if start_date <  appointment_date < end_date:
+                    total_reasons += 1
+                    appointment_reason_object = appointment.get_appointment_reason()
+                    appointment_reason = appointment_reason_object.get_reason()
+                    if appointment_reason not in statistic:
+                        statistic[appointment_reason] = 1
+                    else:
+                        statistic[appointment_reason] += 1
+
+        for key,value in statistic.items():
+            statistic[key] = (value/total_reasons) * 100
+        return statistic
 
     @staticmethod
     def write_appointment(branch_id, appointment_list):
