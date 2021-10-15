f2200cbf6d50e31525c5f2e5c0a7ff6b13dbee8b
Garvin <gtan36@student.monash.edu>
2021-10-15 14:56:22 +1100

    Added comments to admin.py

--- models/admin.py
+++ models/admin.py
@@ -4,11 +4,14 @@ class Admin(User):
     def __init__(self, email_address: str, password: str) -> None:
         User.__init__(self, email_address, password)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return "Admin"
 
     @staticmethod
     def create_from_json(json_info):
+        '''
+        Create an Admin instance based on json input
+        '''
         email_address = json_info["email_address"]
         password = json_info["password"]
         return Admin(email_address, password)
