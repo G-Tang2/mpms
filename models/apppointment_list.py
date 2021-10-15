544391810ecfcf424d5bb168c9efe871787e4e18
Garvin <gtan36@student.monash.edu>
2021-10-15 20:49:13 +1100

    Added comments to appointment_list.py

--- models/apppointment_list.py
+++ models/apppointment_list.py
@@ -1,3 +1,4 @@
+import json
 from typing import List
 from models.appointment import Appointment
 from utils.json import JSON
@@ -9,10 +10,14 @@ class AppointmentList():
     def get_appointment_list(self) -> List[Appointment]:
         return self.appointments
 
-    def add_appointment(self, appointment):
+    def add_appointment(self, appointment: Appointment) -> bool:
         self.appointments.append(appointment)
+        return True
 
-    def to_JSON(self):
+    def to_JSON(self) -> json:
+        '''
+        Returns a json object from this AppointmentList instance
+        '''
         lst = []
         for appointment in self.appointments:
             lst.append(appointment.serialise_copy())
@@ -20,6 +25,9 @@ class AppointmentList():
 
     @staticmethod
     def create_from_json(json_info):
+        '''
+        Create an Appointment instance based on json input
+        '''
         appointment_list = []
         for appointment_json in json_info["appointments"]:
             appointment_list.append(Appointment.create_from_json(appointment_json))
