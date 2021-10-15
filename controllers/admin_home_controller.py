7523f5ccc27e139a1672f2ba3fff69a713458256
Garvin <gtan36@student.monash.edu>
2021-10-16 00:01:53 +1100

    Add type hinting to admin_home_controller.py

--- controllers/admin_home_controller.py
+++ controllers/admin_home_controller.py
@@ -5,17 +5,17 @@ from models.MPMS import MPMS
 
 
 class AdminHomeController(Controller):
-    def __init__(self,master):
+    def __init__(self,master) -> None:
         super().__init__(master)
         self.MPMS = MPMS.get_instance()
         self._view = AdminHomeView(master, self)
         self._initialise_view()
         self._load_view()
 
-    def _initialise_view(self):
+    def _initialise_view(self) -> None:
         login = self.MPMS.get_login()
         user_name = login.get_user_name()
         self._view.render_view(user_name)
 
-    def status_report(self):
+    def status_report(self) -> None:
         self._master.load_controller(StatusReportController)
