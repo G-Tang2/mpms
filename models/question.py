92c7082fd5847b08676502efa42b04d587624d1b
Garvin <gtan36@student.monash.edu>
2021-10-15 23:15:06 +1100

    Added extra comment to question.py

--- models/question.py
+++ models/question.py
@@ -4,17 +4,21 @@ class Question:
         self.question = question
         self.answer = answer
 
-    def get_question(self):
+    def get_question(self) -> str:
         return self.question
 
-    def get_answer(self):
+    def get_answer(self) -> str:
         return self.answer
 
-    def set_answer(self, answer):
+    def set_answer(self, answer) -> bool:
         self.answer = answer
+        return True
 
     @staticmethod
     def create_from_json(json_info):
+        '''
+        Create a question instance based on json input
+        '''
         question = json_info["question"]
         answer = json_info["answer"]
         return Question(question, answer)
