093cf5196f275b416e051eb3e9f28531921a065d
Garvin <gtan36@student.monash.edu>
2021-10-16 00:06:30 +1100

    Add type hinting to patient_home_controller.py

--- controllers/patient_home_controller.py
+++ controllers/patient_home_controller.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 from models.MPMS import MPMS
 from controllers.controller import Controller
 from views.patient_home_view import PatientHomeView
@@ -5,19 +6,19 @@ from controllers.appointment_booking_controller import AppointmentBookingControl
 
 
 class PatientHomeController(Controller):
-    def __init__(self, master):
+    def __init__(self, master: tk.Tk) -> None:
         super().__init__(master)
         self.MPMS = MPMS.get_instance()
         self._view = PatientHomeView(master, self)
         self._initialise_view()
         self._load_view()
 
-    def _initialise_view(self):
+    def _initialise_view(self) -> None:
         login = self.MPMS.get_login()
         user_name = login.get_user_name()
         self._view.render_view(user_name)
 
-    def book_appointment(self):
+    def book_appointment(self) -> None:
         '''
         Loads appointment booking view and controls
         '''
